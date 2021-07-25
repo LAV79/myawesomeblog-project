74153086271ea41fd7588cf9a1156a10d04a0b99
Lobzov Alexey <lobzov_lav@mail.ru>
2021-07-26 00:09:36 +0500

    First commit

--- myawesomeblog/settings.py
+++ myawesomeblog/settings.py
@@ -31,7 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'events.apps.EventsConfig',
+    'blog.apps.BlogConfig', # приложение блога
+    'events.apps.EventsConfig', # приложение события
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
