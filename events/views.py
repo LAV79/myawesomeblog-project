c5c165a800a138f0c42c52008e1dfa2d8df2814e
Lobzov Alexey <lobzov_lav@mail.ru>
2021-07-26 02:13:08 +0500

    First commit

--- events/views.py
+++ events/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Event
 
 # Create your views here.
+def home(rq):
+	evs=Event.objects
+	return render(rq,'events/home.html',{'events':evs})
--- myawesomeblog/urls.py
+++ myawesomeblog/urls.py
@@ -17,7 +17,9 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+import events.views as ev
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', ev.home, name='home'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
