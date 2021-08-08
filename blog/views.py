d469a66c9c242ca69c1ffa2c11f2c41f1e61bc62
Lobzov Alexey <lobzov_lav@mail.ru>
2021-08-09 00:38:26 +0500

    Add posts in blog

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+
+def showblog(rq):
+	posts = Post.objects
+	return render(rq,'blog/blog.html',{'posts':posts})
