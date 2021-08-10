7407cd86955cd62abdcc1bb50593f974d5e3445f
Lobzov Alexey <lobzov_lav@mail.ru>
2021-08-10 23:45:22 +0500

    Finally

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 # Create your views here.
@@ -6,3 +6,7 @@ from .models import Post
 def showblog(rq):
 	posts = Post.objects
 	return render(rq,'blog/blog.html',{'posts':posts})
+
+def specific_post(rq,post_id):	
+	post=get_object_or_404(Post, pk=post_id)
+	return render(rq,'blog/specific_post.html',{'post':post})
